029e3081b9a306ad1c5e0a65026e27b32fe23973
Marvin Bürkner <marvin.buerkner@gmail.com>
2015-05-28 09:30:36 +0200

    Dev

--- WorkHelpers/Form1.cs
+++ WorkHelpers/Form1.cs
@@ -41,15 +41,36 @@ namespace WorkHelpers
             Novacode.Table t = doc.AddTable(table.Cokumns.Count+1, 5);
             int index = 0;
 
+
+            System.Drawing.Font MyFont = new System.Drawing.Font(new System.Drawing.FontFamily("Arial"), 8, FontStyle.Bold | FontStyle.Italic,     GraphicsUnit.Pixel);
+
             t.Rows[index].Cells[0].Paragraphs.First().Append("Spalte");
             t.Rows[index].Cells[1].Paragraphs.First().Append("SCDHash");
             t.Rows[index].Cells[2].Paragraphs.First().Append("Datentyp");
             t.Rows[index].Cells[3].Paragraphs.First().Append("nullable");
-            t.Rows[index++].Cells[4].Paragraphs.First().Append("Quellspalte");
+            t.Rows[index].Cells[4].Paragraphs.First().Append("Quellspalte").Direction=Direction.RightToLeft;
+
+            t.Rows[index].Cells[0].Paragraphs.First().Font(new System.Drawing.FontFamily("Arial"));
+            t.Rows[index].Cells[0].Paragraphs.First().FontSize(8);
+            t.Rows[index].Cells[0].Paragraphs.First().StyleName = "Italic";
+
+            t.Rows[index].Cells[1].Paragraphs.First().Font(new System.Drawing.FontFamily("Arial"));
+            t.Rows[index].Cells[1].Paragraphs.First().FontSize(8);
+
+            t.Rows[index].Cells[2].Paragraphs.First().Font(new System.Drawing.FontFamily("Arial"));
+            t.Rows[index].Cells[2].Paragraphs.First().FontSize(8);
+
+            t.Rows[index].Cells[3].Paragraphs.First().Font(new System.Drawing.FontFamily("Arial"));
+            t.Rows[index].Cells[3].Paragraphs.First().FontSize(8);
+
+            t.Rows[index].Cells[4].Paragraphs.First().Font( new System.Drawing.FontFamily("Arial"));
+            t.Rows[index].Cells[4].Paragraphs.First().FontSize(8);
+
+            index++;
             foreach (Column column in table.Cokumns)
             {
                 t.Rows[index].Cells[0].Paragraphs.First().Append(column.Name);
-                t.Rows[index++].Cells[2].Paragraphs.First().Append(column.Datatyp);
+                t.Rows[index++].Cells[2].Paragraphs.First().Append((column.Datatyp).ToUpper());
             }
             doc.InsertTable(t);
 
--- WorkHelpers/Klassen/SQLHelper.cs
+++ WorkHelpers/Klassen/SQLHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace WorkHelpers.Klassen
@@ -10,7 +11,7 @@ namespace WorkHelpers.Klassen
     {
         public static Table getTableFromCreateStatement(string createStatement) {
             Table table = new Table();
-            string[] columns = createStatement.Split(',');
+            string[] columns = Regex.Split(createStatement, "\n");// createStatement.Split(',');
             foreach (string s in columns)
             {
                 if (s.Contains("(") && s.Contains("CREATE"))
@@ -21,12 +22,25 @@ namespace WorkHelpers.Klassen
                     string[] x = w[0].Split('.');//[dbo].[DimCountry]
                     table.Schema = StringHelper.cleanupSQL(x[1]);
                     table.Name = StringHelper.cleanupSQL(x[0]);
-                    Column column = getColumnInfos(w[1] + " " + werte[3] + " " + werte[4]);
+                    try
+                    {
+                        string c = w[1] + " " + werte[3] + " " + werte[4];
+                        if (c.Length > 3)
+                        {
+                            Column column = getColumnInfos(c);
+                        }
+                    }
+                    catch 
+                    {
+                    }
                 }
                 else
                 {
-                    Column column = getColumnInfos(s);
-                    table.Cokumns.Add(column);
+                    if (s.Length > 3)
+                    {
+                        Column column = getColumnInfos(s);
+                        table.Cokumns.Add(column);
+                    }
                 }
             }
             return table;
@@ -35,6 +49,11 @@ namespace WorkHelpers.Klassen
         private static Column getColumnInfos(string s)
         {
             s = StringHelper.cleanupSQL(s);
+            //[(]{1}[ ]*[0-9]{1,2}[ ]*[,]{1}[ ]*[0-9]{1,2}[ ]*[)]
+            s=Regex.Replace(s,"[ ]*[)]",")");//(9,6 )->(9,6)
+            s=Regex.Replace(s, "[(][ ]*","(");//( 9,6)->(9,6)
+            s = Regex.Replace(s, "[,][ ]*", ",");//(9, 6)->(9,6)
+            s = Regex.Replace(s, "[ ]*[,]", ",");//(9 ,6)->(9,6)
             string[] werte = s.Split(' ');
             Column column = new Column();
             column.Name = werte[0];
